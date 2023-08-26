4e9b0899126e511a0514e1b27c627d5499dfff96
Atabek Otepbergenov <atabek122002@gmail.com>
2023-08-26 10:22:55 +0300

    Исправил баги

--- src/components/screens/user/Service/ServicePage.tsx
+++ src/components/screens/user/Service/ServicePage.tsx
@@ -1,5 +1,9 @@
 import React from 'react';
 
-const ServicePage: React.FC = () => <div>ServicePages</div>;
+const ServicePage: React.FC = () => (
+  <div>
+    <div className="title">Наши услуги</div>
+  </div>
+);
 
 export { ServicePage };

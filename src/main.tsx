cc893ac8b84a84517aba67581423d6a5e68f3a3a
Atabek Otepbergenov <atabek122002@gmail.com>
2023-08-28 09:10:01 +0300

    Исправил баги

--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,8 @@ import ReactDOM from 'react-dom/client';
 import { Provider } from 'react-redux';
 import { BrowserRouter as Router } from 'react-router-dom';
 
+import 'swiper/css';
+
 import { App } from './App';
 import { store } from './store';
 

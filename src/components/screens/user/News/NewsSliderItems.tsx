d350f9d2c44350db6297adbe5e8ce66126980574
Atabek Otepbergenov <atabek122002@gmail.com>
2023-08-29 15:15:46 +0300

    Исправил баги

--- src/components/screens/user/News/NewsSliderItems.tsx
+++ src/components/screens/user/News/NewsSliderItems.tsx
@@ -1,44 +1,31 @@
 import React from 'react';
+import { useNavigate } from 'react-router-dom';
 import Slider from 'react-slick';
 import { UiButton } from 'src/components/ui';
+import { newsSettings } from 'src/config/slider.config';
 
 import { NewsItem } from './NewsItem';
 
 import s from './news.module.scss';
 
 const NewsSliderItems: React.FC = () => {
-  const settings = {
-    dots: true,
-    infinite: true,
-    speed: 500,
-    slidesToShow: 3,
-    slidesToScroll: 3,
-    initialSlide: 0,
-    responsive: [
-      {
-        breakpoint: 700,
-        settings: {
-          slidesToShow: 2,
-          slidesToScroll: 2,
-        },
-      },
-      {
-        breakpoint: 580,
-        settings: {
-          slidesToShow: 1,
-          slidesToScroll: 1,
-        },
-      },
-    ],
-  };
+  const navigate = useNavigate();
   return (
     <div className={s.sliders}>
-      <Slider {...settings}>
+      <Slider {...newsSettings}>
         {[...Array(10)].map((_, i) => (
           <NewsItem direction="vertical" key={i} />
         ))}
       </Slider>
-      <UiButton type="default" color="blue" text="Все новости" icon />
+      <div className={s.btn}>
+        <UiButton
+          type="default"
+          icon
+          color="blue"
+          text="Все новости"
+          onClick={() => navigate('/news')}
+        />
+      </div>
     </div>
   );
 };
---
+++ src/config/slider.config.ts
@@ -0,0 +1,23 @@
+export const newsSettings = {
+  dots: true,
+  infinite: true,
+  speed: 500,
+  slidesToShow: 3,
+  slidesToScroll: 3,
+  responsive: [
+    {
+      breakpoint: 700,
+      settings: {
+        slidesToShow: 2,
+        slidesToScroll: 2,
+      },
+    },
+    {
+      breakpoint: 580,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1,
+      },
+    },
+  ],
+};

6f075869e8dd2d5c65f335a32e480ca1646d00f6
Atabek Otepbergenov <atabek122002@gmail.com>
2023-10-04 11:49:52 +0300

    Исправил баги

--- src/components/screens/user/Home/HomeSlider.tsx
+++ src/components/screens/user/Home/HomeSlider.tsx
@@ -1,11 +1,12 @@
 import React from 'react';
 import Img from 'react-cool-img';
 import Slider from 'react-slick';
-import Banner from 'src/assets/images/slider.jpg';
+import { useGetUserSlidersQuery } from 'src/services';
 
 import s from './home.module.scss';
 
 const HomeSlider: React.FC = () => {
+  const { data: sliders } = useGetUserSlidersQuery();
   const settings = {
     dots: false,
     infinite: true,
@@ -15,11 +16,16 @@ const HomeSlider: React.FC = () => {
     slidesToScroll: 1,
   };
   return (
-    <div className={s.slider}>
-      <Slider {...settings} className={s.sliders}>
-        <Img src={Banner} alt="Banner" />
-        <Img src={Banner} alt="Banner" />
-      </Slider>
+    <div>
+      {sliders?.data.length ? (
+        <div className={s.slider}>
+          <Slider {...settings} className={s.sliders}>
+            {sliders?.data.map((slider) => (
+              <Img src={slider.image.image_url} alt={slider.title} />
+            ))}
+          </Slider>
+        </div>
+      ) : null}
     </div>
   );
 };
